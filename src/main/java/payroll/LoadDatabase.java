ffa61c7c67a23a9f132354a81c7f36e5f973f7c5
Jan Zemlicka <zemlicka@mobisec.com>
2024-07-23 09:16:34 +0200

    Added comments for unknown functions.

--- src/main/java/payroll/LoadDatabase.java
+++ src/main/java/payroll/LoadDatabase.java
@@ -9,11 +9,14 @@ import org.slf4j.LoggerFactory;
 @Configuration
 class LoadDatabase {
 
+    //once the application is loaded Spring Boot will run all commandline runners.
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
+    //requests a copy of the repository
     @Bean
     CommandLineRunner initDatabase(EmployeeRepository repository) {
 
+        //the runner creates two entities and stores them
         return args -> {
             log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
             log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
