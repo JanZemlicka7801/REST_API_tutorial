6be1404f5958b6467f930a70c498716076bfc390
Jan Zemlicka <zemlicka@mobisec.com>
2024-07-24 12:17:58 +0200

    Replaced all with EntityModel<Employee> creation logic with map(assembler::toModel)

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -38,11 +38,10 @@ class EmployeeController {
     */
 
     @GetMapping("/employees")
-    //collection of resources, also can include links (collection of employee resources)
     CollectionModel<EntityModel<Employee>> all() {
-        List<EntityModel<Employee>> employees = repository.findAll().stream().map(employee -> EntityModel.of(employee,
-                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+
+        List<EntityModel<Employee>> employees = repository.findAll().stream() //
+                .map(assembler::toModel) //
                 .collect(Collectors.toList());
 
         return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
